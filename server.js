090279c2568ad8518c1ee89fe5d017922c60cc5d
Ignacio Allendes <igna.allendes@gmail.com>
2026-09-07 15:28:34 -0300

    Feat: Routes para auth, mesa, salon y solicitud

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
-app.use(express.json())
+// Importar rutas
+import authRoutes from './routes/auth.routes.js';
+import salonRoutes from './routes/salon.routes.js';
+import mesaRoutes from './routes/mesa.routes.js';
+import solicitudRoutes from './routes/solicitud.routes.js';
+
+const app = express();
+app.use(express.json());
+
+// Montar endpoints API
+app.use('/api/auth', authRoutes);
+app.use('/api/salones', salonRoutes);
+app.use('/api/mesas', mesaRoutes);
+app.use('/api/solicitudes', solicitudRoutes);
 
 // Ruta de prueba
 app.get('/', (req, res) => {
-  res.json({ mensaje: '¡Servidor del proyecto Garzón funcionando!' })
-})
+  res.json({ mensaje: '¡Servidor del proyecto Garzón funcionando!' });
+});
 
-app.listen(3001, () => console.log('API en http://localhost:3001'))
+app.listen(3001, () => console.log('API en http://localhost:3001'));
